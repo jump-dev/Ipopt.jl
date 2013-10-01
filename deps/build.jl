6c4131cea941c327e85a15135926dc0a17648646
Miles Lubin <miles.lubin@gmail.com>
2013-10-01 12:42:16 -0400

    binary work

---
+++ deps/build.jl
@@ -0,0 +1,27 @@
+using BinDeps
+
+@BinDeps.setup
+
+libipopt = library_dependency("libipopt")
+
+ipoptname = "Ipopt-3.11.4"
+
+provides(Sources, URI("http://www.coin-or.org/download/source/Ipopt/$ipoptname.tgz"),
+    libipopt, os = :Unix)
+
+prefix=joinpath(BinDeps.depsdir(libipopt),"usr")
+patchdir=BinDeps.depsdir(libipopt)
+srcdir = joinpath(BinDeps.depsdir(libipopt),"src",ipoptname)
+
+provides(SimpleBuild,
+    (@build_steps begin
+        GetSources(libipopt)
+        @build_steps begin
+            ChangeDirectory(srcdir)
+            `cat $patchdir/ipopt-shlibs.patch` |> `patch -p1`
+            `./configure --prefix=$prefix`
+            `make install`
+        end
+    end),libipopt, os = :Unix)
+
+@BinDeps.install
--- src/Ipopt.jl
+++ src/Ipopt.jl
@@ -1,5 +1,6 @@
 module Ipopt
 
-# package code goes here
+using BinDeps
+@BinDeps.load_dependencies
 
 end # module
