28278bfc747f28ec55215d1dadc7b10e07adc8d1
Iain Dunning <iaindunning@gmail.com>
2013-10-12 19:26:09 -0400

    No ccalls in user code.

--- src/Ipopt.jl
+++ src/Ipopt.jl
@@ -4,4 +4,90 @@ module Ipopt
   @BinDeps.load_dependencies
   
   export libipopt
+  export CreateProblem, AddOption, FreeProblem, SolveProblem
+
+  type IpoptProblem
+    ref::Ptr{Void}
+    n::Int
+    x::Vector{Float64}
+    obj_val::Float64
+    
+    function IpoptProblem(ref::Ptr{Void}, n)
+      prob = new(ref, n, zeros(Float64, n), 0.0)
+      finalizer(prob, FreeProblem)
+      prob
+    end
+  end
+
+
+  function CreateProblem(n::Int, x_L::Vector{Float64}, x_U::Vector{Float64},
+                              m::Int, g_L::Vector{Float64}, g_U::Vector{Float64},
+                              nele_jac::Int, nele_hess::Int, 
+                              eval_f, eval_g, eval_grad_f, eval_jac_g, eval_h)
+    # Wrap callbacks
+    eval_f_cb = cfunction(eval_f, Cint, (Cint, Ptr{Float64}, Cint, Ptr{Float64}, Ptr{Void}))
+    eval_g_cb = cfunction(eval_g, Cint, (Cint, Ptr{Float64}, Cint, Cint, Ptr{Float64}, Ptr{Void}))
+    eval_grad_f_cb = cfunction(eval_grad_f, Cint, (Cint, Ptr{Float64}, Cint, Ptr{Float64}, Ptr{Void}))
+    eval_jac_g_cb = cfunction(eval_jac_g, Cint, (Cint, Ptr{Float64}, Cint, Cint, Cint, Ptr{Cint}, Ptr{Cint}, Ptr{Float64}, Ptr{Void}))
+    eval_h_cb = cfunction(eval_h, Cint, (Cint, Ptr{Float64}, Cint, Float64, Cint, Ptr{Float64}, Cint, Cint, Ptr{Cint}, Ptr{Cint}, Ptr{Float64}, Ptr{Void}))
+
+    ret = ccall((:CreateIpoptProblem, libipopt), Ptr{Void},
+        (Cint, Ptr{Float64}, Ptr{Float64},  # Num vars, var lower and upper bounds
+         Cint, Ptr{Float64}, Ptr{Float64},  # Num constraints, con lower and upper bounds
+         Cint, Cint, # Num nnz in constraint Jacobian and in "Hessian of Lagrangian"
+         Cint, # 0 for C, 1 for Fortran
+         Ptr{Void}, Ptr{Void}, # Callbacks for eval_f, eval_g
+         Ptr{Void}, Ptr{Void}, Ptr{Void}), # Callbacks for eval_grad_f, eval_jac_g, eval_h
+         n, x_L, x_U, m, g_L, g_U, nele_jac, nele_hess, 0,
+         eval_f_cb, eval_g_cb, eval_grad_f_cb, eval_jac_g_cb, eval_h_cb)
+
+    if ret == C_NULL
+      error("IPOPT: Failed to construct problem.")
+    else
+      return(IpoptProblem(ret, n))
+    end
+  end
+
+  function FreeProblem(prob::IpoptProblem)
+    ccall((:FreeIpoptProblem, libipopt), Void, (Ptr{Void},), prob.ref)
+  end
+
+  function AddOption(prob::IpoptProblem, keyword::ASCIIString, value::ASCIIString)
+    ret = ccall((:AddIpoptStrOption, libipopt), 
+                Cint, (Ptr{Void}, Ptr{Uint8}, Ptr{Uint8}),
+                prob.ref, keyword, value)
+    if ret == 0
+      error("IPOPT: Couldn't set option '$keyword' to value '$value'.")
+    end
+  end
+
+  function AddOption(prob::IpoptProblem, keyword::ASCIIString, value::Float64)
+    ret = ccall((:AddIpoptIntOption, libipopt),
+                Cint, (Ptr{Void}, Ptr{Uint8}, Float64),
+                prob.ref, keyword, value)
+    if ret == 0
+      error("IPOPT: Couldn't set option '$keyword' to value '$value'.")
+    end
+  end
+
+  function AddOption(prob::IpoptProblem, keyword::ASCIIString, value::Integer)
+    ret = ccall((:AddIpoptIntOption, libipopt),
+                Cint, (Ptr{Void}, Ptr{Uint8}, Cint),
+                prob.ref, keyword, value)
+    if ret == 0
+      error("IPOPT: Couldn't set option '$keyword' to value '$value'.")
+    end
+  end
+
+  # TODO: Expose full functionality
+  function SolveProblem(prob::IpoptProblem)
+    final_objval = [0.0]
+    ret = ccall((:IpoptSolve, libipopt),
+                Ptr{Void}, (Ptr{Void}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Void}),
+                prob.ref, prob.x, C_NULL, final_objval, C_NULL, C_NULL, C_NULL, C_NULL)
+
+    prob.obj_val = final_objval[1]
+  end
+
+
 end # module
--- test/hs071.jl
+++ test/hs071.jl
@@ -1,9 +1,5 @@
 using Ipopt
 
-type IpoptProblem
-  ref::Ptr{Void}
-end
-
 # hs071
 # min x1 * x4 * (x1 + x2 + x3) + x3
 # st  x1 * x2 * x3 * x4 >= 25
@@ -11,15 +7,6 @@ end
 #     1 <= x1, x2, x3, x4 <= 5
 # Start at (1,5,5,1)
 # End at (1.000..., 4.743..., 3.821..., 1.379...)
-
-n = 4
-x_L = [1.0, 1.0, 1.0, 1.0]
-x_U = [5.0, 5.0, 5.0, 5.0]
-
-m = 2
-g_L = [25.0, 40.0]
-g_U = [2.0e19, 40.0]
-
 
 function eval_f(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, obj_ptr::Ptr{Float64}, user_data::Ptr{Void})
   println("eval_f")
@@ -29,7 +16,7 @@ function eval_f(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, obj_ptr::Ptr{Float64}
   # new_x: (in), false if any evaluation method was previously called with the same values in x, true otherwise.
   # obj_value: (out) the value of the objective function ($ f(x)$ ).
   
-  x = pointer_to_array(x_ptr, n)
+  x = pointer_to_array(x_ptr, n*1)
 
   new_obj = x[1] * x[4] * (x[1] + x[2] + x[3]) + x[3]
 
@@ -37,8 +24,6 @@ function eval_f(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, obj_ptr::Ptr{Float64}
 
   return int32(1)
 end
-eval_f_cb = cfunction(eval_f, Cint, (Cint, Ptr{Float64}, Cint, Ptr{Float64}, Ptr{Void}))
-
 
 function eval_g(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, m::Cint, g::Ptr{Float64}, user_data::Ptr{Void})
   println("eval_g")
@@ -49,15 +34,13 @@ function eval_g(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, m::Cint, g::Ptr{Float
   # m: (in), the number of constraints in the problem (dimension of $ g(x)$ ).
   # g: (out) the array of constraint function values, $ g(x)$ .
 
-  x = pointer_to_array(x_ptr, n)
+  x = pointer_to_array(x_ptr, n*1)
 
   unsafe_store!(g, x[1] * x[2] * x[3] * x[4], 1)
   unsafe_store!(g, x[1]*x[1] + x[2]*x[2] + x[3]*x[3] + x[4]*x[4], 2)
 
   return int32(1)
 end
-eval_g_cb = cfunction(eval_g, Cint, (Cint, Ptr{Float64}, Cint, Cint, Ptr{Float64}, Ptr{Void}))
-
 
 function eval_grad_f(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, grad_f::Ptr{Float64}, user_data::Ptr{Void})
   println("eval_grad_f")
@@ -66,8 +49,8 @@ function eval_grad_f(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, grad_f::Ptr{Floa
   # x: (in), the values for the primal variables, $ x$ , at which  $ \nabla f(x)$ is to be evaluated.
   # new_x: (in), false if any evaluation method was previously called with the same values in x, true otherwise.
   # grad_f: (out) the array of values for the gradient of the objective function ( $ \nabla f(x)$ ).
-
-  x = pointer_to_array(x_ptr, n)
+  
+  x = pointer_to_array(x_ptr, (n*1,))
 
   unsafe_store!(grad_f, x[1] * x[4] + x[4] * (x[1] + x[2] + x[3]), 1)
   unsafe_store!(grad_f, x[1] * x[4], 2)
@@ -76,8 +59,6 @@ function eval_grad_f(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, grad_f::Ptr{Floa
   
   return int32(1)
 end
-eval_grad_f_cb = cfunction(eval_grad_f, Cint, (Cint, Ptr{Float64}, Cint, Ptr{Float64}, Ptr{Void}))
-
 
 function eval_jac_g(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, m::Cint, nele_jac::Cint, iRow::Ptr{Cint}, jCol::Ptr{Cint}, values::Ptr{Float64}, user_data::Ptr{Void})
   println("eval_jac_g")
@@ -130,7 +111,7 @@ function eval_jac_g(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, m::Cint, nele_jac
 
   return int32(1)
 end
-eval_jac_g_cb = cfunction(eval_jac_g, Cint, (Cint, Ptr{Float64}, Cint, Cint, Cint, Ptr{Cint}, Ptr{Cint}, Ptr{Float64}, Ptr{Void}))
+
 
 
 function eval_h(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, obj_factor::Float64, m::Cint, lamda::Ptr{Float64}, new_lambda::Cint, nele_hess::Cint, iRow::Ptr{Cint}, jCol::Ptr{Cint}, values::Ptr{Float64}, user_data::Ptr{Void})
@@ -138,24 +119,23 @@ function eval_h(n::Cint, x_ptr::Ptr{Float64}, new_x::Cint, obj_factor::Float64, 
   # Do it later
   return int32(0)
 end
-eval_h_cb = cfunction(eval_h, Cint, (Cint, Ptr{Float64}, Cint, Float64, Cint, Ptr{Float64}, Cint, Cint, Ptr{Cint}, Ptr{Cint}, Ptr{Float64}, Ptr{Void}))
 
 
-ret = ccall((:CreateIpoptProblem, libipopt), Ptr{Void},
-        (Cint, Ptr{Float64}, Ptr{Float64},  # Num vars, var lower and upper bounds
-         Cint, Ptr{Float64}, Ptr{Float64},  # Num constraints, con lower and upper bounds
-         Cint, Cint, # Num nnz in constraint Jacobian and in "Hessian of Lagrangian"
-         Cint, # 0 for C, 1 for Fortran
-         Ptr{Void}, Ptr{Void}, Ptr{Void}, Ptr{Void}, Ptr{Void}), # Callbacks for eval_f, eval_g, eval_grad_f, eval_jac_g, eval_h
-         n, x_L, x_U, m, g_L, g_U, 8, 10, 0,
-         eval_f_cb, eval_g_cb, eval_grad_f_cb, eval_jac_g_cb, eval_h_cb)
-println(ret)
-prob = IpoptProblem(ret)
 
-x = [1.0, 5.0, 5.0, 1.0]
-objval = [0.0]
-ret = ccall((:IpoptSolve, libipopt), Ptr{Void}, (Ptr{Void}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Void}), prob.ref, x, C_NULL, objval, C_NULL, C_NULL, C_NULL, C_NULL)
+n = 4
+x_L = [1.0, 1.0, 1.0, 1.0]
+x_U = [5.0, 5.0, 5.0, 5.0]
 
-println(ret)
-println(x)
-println(objval)
+m = 2
+g_L = [25.0, 40.0]
+g_U = [2.0e19, 40.0]
+
+prob = CreateProblem(n, x_L, x_U, m, g_L, g_U, 8, 10,
+                     eval_f, eval_g, eval_grad_f, eval_jac_g, eval_h)
+
+AddOption(prob, "hessian_approximation", "limited-memory")
+prob.x = [1.0, 5.0, 5.0, 1.0]
+SolveProblem(prob)
+
+println(prob.x)
+println(prob.obj_val)
