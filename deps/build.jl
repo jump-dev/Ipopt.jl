0849474d9ed899ba6f8f0193147615fbc0df8843
Miles Lubin <miles.lubin@gmail.com>
2013-10-16 18:44:10 -0400

    Windows support Binaries for both 32- and 64-bit available, but they cannot coexist in the same install. Users need to delete the deps/usr directory when switching between 32- and 64-bit julia.

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,8 @@ using BinDeps
 
 @BinDeps.setup
 
-libipopt = library_dependency("libipopt")
+windllname = "IpOpt-vc10"
+libipopt = library_dependency("libipopt", aliases=[windllname])
 
 ipoptname = "Ipopt-3.11.4"
 
@@ -28,4 +29,28 @@ provides(SimpleBuild,
         end
     end),libipopt, os = :Unix)
 
+# Windows
+downloadsdir = BinDeps.downloadsdir(libipopt)
+libdir = BinDeps.libdir(libipopt)
+downloadname = "Ipopt-3.11.0-Win32-Win64-dll.7z"
+windir = WORD_SIZE == 32 ? "Win32" : "x64"
+
+provides(BuildProcess,
+    (@build_steps begin
+        FileDownloader("http://www.coin-or.org/download/binary/Ipopt/$downloadname", joinpath(downloadsdir, downloadname))
+	CreateDirectory(BinDeps.srcdir(libipopt), true)
+	FileUnpacker(joinpath(downloadsdir, downloadname), BinDeps.srcdir(libipopt), joinpath(BinDeps.srcdir(libipopt),"lib","Win32"))
+	CreateDirectory(libdir, true)
+	@build_steps begin
+	    ChangeDirectory(joinpath(BinDeps.srcdir(libipopt),"lib",windir,"ReleaseMKL"))
+	    FileRule(joinpath(libdir,"$(windllname).dll"), @build_steps begin
+	        `cp *.dll $(libdir)`
+	    end)
+	end
+     end), libipopt, os = :Windows)
+
+@windows_only push!(BinDeps.defaults, BuildProcess)
+
 @BinDeps.install
+
+@windows_only pop!(BinDeps.defaults)
