69fb7e14ebe20c6fb2616fc3f91aa480c9002610
Elliot Saba <staticfloat@gmail.com>
2014-10-18 21:44:22 -0700

    Fix @test_throws deprecation warning

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,15 +8,15 @@ include("hs071_test.jl")
 # String option
 println("\nString option")
 addOption(prob, "hessian_approximation", "exact")
-@test_throws addOption(prob, "hessian_approximation", "badoption")
+@test_throws ErrorException addOption(prob, "hessian_approximation", "badoption")
 println("\nInt option")
 # Int option
 addOption(prob, "file_print_level", 3) == nothing
-@test_throws addOption(prob, "file_print_level", -1)
+@test_throws ErrorException addOption(prob, "file_print_level", -1)
 # Double option
 println("\nFloat option")
 addOption(prob, "derivative_test_tol", 0.5)
-@test_throws addOption(prob, "derivative_test_tol", -1.0)
+@test_throws ErrorException addOption(prob, "derivative_test_tol", -1.0)
 
 # Test opening an output file
 openOutputFile(prob, "blah.txt", 5)
