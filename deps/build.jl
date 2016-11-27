acb1a5b903265055227b2cae5ec1c90df9ebc1e9
Miles Lubin <miles.lubin@gmail.com>
2016-11-27 09:00:05 -0500

    another way to work around travis failures

--- deps/build.jl
+++ deps/build.jl
@@ -30,7 +30,6 @@ provides(SimpleBuild,
             @build_steps begin
                 ChangeDirectory(joinpath(srcdir,"ThirdParty","Blas"))
                 CreateDirectory("build", true)
-                `sed -i 's/wget"/wget --no-check-certificate"/g' get.Blas`
                 `./get.Blas`
             end
             @build_steps begin
@@ -41,7 +40,6 @@ provides(SimpleBuild,
             @build_steps begin
                 ChangeDirectory(joinpath(srcdir,"ThirdParty","Lapack"))
                 CreateDirectory("build", true)
-                `sed -i 's/wget"/wget --no-check-certificate"/g' get.Lapack`
                 `./get.Lapack`
             end
             @build_steps begin
