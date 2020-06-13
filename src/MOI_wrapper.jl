f7f3eb037d3c26fb3b54b1c388fdb288e89820ec
Akshay Sharma <contact@imakshay.com>
2020-06-13 23:54:01 +0530

    few more attributes

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -179,6 +179,56 @@ function MOI.get(model::Optimizer, ::MOI.ListOfConstraints)
 end
 
 
+function MOI.get(
+    model::Ipopt.Optimizer,
+    ::MOI.ListOfConstraintIndices{MOI.ScalarAffineFunction{Float64}, MOI.LessThan{Float64}}
+) 
+    indices = MOI.ConstraintIndex{MOI.ScalarAffineFunction{Float64}, MOI.LessThan{Float64}}[]
+    n = size(model.linear_le_constraints)[1]
+    for i in 1:n 
+        push!(indices, MOI.ConstraintIndex{MOI.ScalarAffineFunction{Float64}, MOI.LessThan{Float64}}(i))
+    end
+    return indices
+end
+
+
+function MOI.get(
+    model::Ipopt.Optimizer,
+    ::MOI.ListOfConstraintIndices{MOI.ScalarAffineFunction{Float64}, MOI.EqualTo{Float64}}
+) 
+    indices = MOI.ConstraintIndex{MOI.ScalarAffineFunction{Float64}, MOI.EqualTo{Float64}}[]
+    n = size(model.linear_eq_constraints)[1]
+    for i in 1:n 
+        push!(indices, MOI.ConstraintIndex{MOI.ScalarAffineFunction{Float64}, MOI.EqualTo{Float64}}(i))
+    end
+    return indices
+end
+
+
+function MOI.get(
+    model::Ipopt.Optimizer,
+    ::MOI.ConstraintFunction,
+    c::MOI.ConstraintIndex{MOI.ScalarAffineFunction{Float64}, MOI.LessThan{Float64}}
+) 
+    return model.linear_le_constraints[c.value].func
+end
+
+function MOI.get(
+    model::Ipopt.Optimizer,
+    ::MOI.ConstraintSet,
+    c::MOI.ConstraintIndex{MOI.ScalarAffineFunction{Float64}, MOI.LessThan{Float64}}
+) 
+    return model.linear_le_constraints[c.value].set
+end
+
+function MOI.get(
+    model::Ipopt.Optimizer,
+    ::MOI.ObjectiveFunction
+)
+    return model.objective
+end
+
+
 function MOI.set(model::Optimizer, ::MOI.ObjectiveSense,
                  sense::MOI.OptimizationSense)
     model.sense = sense
