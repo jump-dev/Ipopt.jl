0305068e7181c9575f12f03de9c35fea92c68f6e
odow <o.dowson@gmail.com>
2022-02-16 21:45:42 +1300

    Remove deprecated features

--- src/Ipopt.jl
+++ src/Ipopt.jl
@@ -1,20 +1,10 @@
 module Ipopt
 
-using Libdl
+import Ipopt_jll
 
-if VERSION < v"1.3" || (
-    haskey(ENV, "JULIA_IPOPT_LIBRARY_PATH") &&
-    haskey(ENV, "JULIA_IPOPT_EXECUTABLE_PATH")
-)
-    const _DEPS_FILE = joinpath(dirname(@__DIR__), "deps", "deps.jl")
-    if !isfile(_DEPS_FILE)
-        error(
-            "Ipopt not properly installed. Please run import Pkg; Pkg.build(\"Ipopt\")",
-        )
-    end
-    include(_DEPS_FILE)
-else
-    import Ipopt_jll: libipopt
+function __init__()
+    global libipopt = Ipopt_jll.libipopt
+    return
 end
 
 include("C_wrapper.jl")
