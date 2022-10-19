5fb1c6ea644ed023cec6429ac2efceec4c11a717
Alexis <35051714+amontoison@users.noreply.github.com>
2022-10-19 17:04:43 -0400

    Ipopt_jll with LBT (#327)

--- src/Ipopt.jl
+++ src/Ipopt.jl
@@ -6,11 +6,23 @@
 module Ipopt
 
 import Ipopt_jll
+import LinearAlgebra
 import MathOptInterface
+import OpenBLAS32_jll
 
 const MOI = MathOptInterface
 
 function __init__()
+    if VERSION >= v"1.8"
+        config = LinearAlgebra.BLAS.lbt_get_config()
+        if !any(lib -> lib.interface == :lp64, config.loaded_libs)
+            LinearAlgebra.BLAS.lbt_forward(
+                OpenBLAS32_jll.libopenblas_path;
+                verbose = true,
+                clear = false,
+            )
+        end
+    end
     global libipopt = Ipopt_jll.libipopt
     return
 end
