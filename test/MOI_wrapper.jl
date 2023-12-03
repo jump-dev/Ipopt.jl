04178d8589c6c174cc5cbfb71de2e103ce9c5a06
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2023-12-03 16:36:26 -0600

    Add a test for HSL solvers (#395)

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -5,8 +5,10 @@
 
 module TestMOIWrapper
 
-using Ipopt
 using Test
+
+import HSL_jll
+import Ipopt
 
 const MOI = Ipopt.MOI
 
@@ -491,7 +493,9 @@ function test_ListOfSupportedNonlinearOperators()
 end
 
 function test_SPRAL()
-    if VERSION < v"1.9" || !haskey(ENV, "OMP_CANCELLATION")
+    if VERSION < v"1.9" ||
+       !haskey(ENV, "OMP_CANCELLATION") ||
+       !haskey(ENV, "OMP_PROC_BIND")
         return
     end
     model = Ipopt.Optimizer()
@@ -503,6 +507,24 @@ function test_SPRAL()
     MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
     MOI.optimize!(model)
     @test ≈(MOI.get(model, MOI.VariablePrimal(), x), 2.0; atol = 1e-6)
+    return
+end
+
+function test_HSL()
+    if !(@ccall HSL_jll.libhsl.LIBHSL_isfunctional()::Bool)
+        return
+    end
+    for hsl_solver in ("ma27", "ma57", "ma77", "ma86", "ma97")
+        model = Ipopt.Optimizer()
+        MOI.set(model, MOI.RawOptimizerAttribute("linear_solver"), hsl_solver)
+        MOI.set(model, MOI.Silent(), true)
+        x = MOI.add_variable(model)
+        MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+        f = 1.0 * x * x - 4.0 * x + 4.0
+        MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+        MOI.optimize!(model)
+        @test ≈(MOI.get(model, MOI.VariablePrimal(), x), 2.0; atol = 1e-6)
+    end
     return
 end
 
