23596e60c3a1a3b5b62e8b0b75d79a811970443b
Oscar Dowson <odow@users.noreply.github.com>
2025-10-24 09:49:35 +1300

    Fix computing primal status with a VectorNonlinearOracle (#511)

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -936,6 +936,9 @@ function test_vector_nonlinear_oracle()
         @test 0.9 < cache.eval_jacobian_timer < 4
         @test 0.9 < cache.eval_hessian_lagrangian_timer < 2
     end
+    MOI.set(model, MOI.RawOptimizerAttribute("max_iter"), 0)
+    MOI.optimize!(model)
+    @test MOI.get(model, MOI.PrimalStatus()) == MOI.INFEASIBLE_POINT
     return
 end
 
